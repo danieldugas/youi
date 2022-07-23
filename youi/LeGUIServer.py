9292e9396f3328a6a2b41c5db0860a18b957dcca
Daniel Dugas <exodaniel@gmail.com>
2022-07-23 16:45:09 +0200

    Adds many widget types

--- youi/LeGUIServer.py
+++ youi/LeGUIServer.py
@@ -1,4 +1,5 @@
 # TODO: click to add button
+from plistlib import UID
 import sys
 import time
 import uuid
@@ -10,7 +11,7 @@ from matplotlib import widgets
 matplotlib.use('Qt5Agg')
 
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QWidget, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QWidget, QPushButton, QMainWindow, QLabel, QSlider, QCheckBox
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtWidgets import QInputDialog
@@ -23,32 +24,13 @@ import rospy
 
 from common_srvs.srv import QuestionAnswer, QuestionAnswerResponse
 
-class MplCanvas(FigureCanvasQTAgg):
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
 
-class MainLayoutWidget(QWidget):
-    def __init__(self, color):
-        super(MainLayoutWidget, self).__init__()
-        self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
     
-
-class LeVBox(QWidget):
+class LeLayout(QWidget):
+    DEPTH_COLORS = ["white", "gold", "cyan", "green", "black", "red"]
     """ An empty widget with vertical layout, can be right clicked to add objects inside """
-    def __init__(self, leguiserver, color=None):
-        super(LeVBox, self).__init__()
-        self.leguiserver = leguiserver
-
-        if color is not None:
-            self.setAutoFillBackground(True)
-            palette = self.palette()
-            palette.setColor(QPalette.Window, QColor(color))
-            self.setPalette(palette)
+    def __init__(self, uid, label, parent, leguiserver):
+        raise NotImplementedError
 
     # def mousePressEvent(self, QMouseEvent):
     #     if QMouseEvent.button() == Qt.LeftButton:
@@ -59,45 +41,156 @@ class LeVBox(QWidget):
 
     def contextMenuEvent(self, event):
         contextMenu = QMenu(self)
-        addAct = contextMenu.addAction("Add")
+        empty = contextMenu.addAction(self.get_label())
+        addBtnAct = contextMenu.addAction("Add Button")
+        addPPlotAct = contextMenu.addAction("Add Pyplot")
+        addCheckboxAct = contextMenu.addAction("Add Checkbox")
+        addSliderAct = contextMenu.addAction("Add Slider")
+        addLabelAct = contextMenu.addAction("Add Label")
+        addVBoxAct = contextMenu.addAction("Add Vertical Layout")
+        addHBoxAct = contextMenu.addAction("Add Horizontal Layout")
+        rmSelfAct = contextMenu.addAction("Delete Layout")
         action = contextMenu.exec_(self.mapToGlobal(event.pos()))
-        if action == addAct:
-            self.addLeButton()
-            # self.close()
+        if action == addBtnAct:
+            self.addLeWidget(LeButton, uid=None)
+        if action == addVBoxAct:
+            self.addLeWidget(LeVBox, uid=None)
+        if action == addHBoxAct:
+            self.addLeWidget(LeHBox, uid=None)
+        if action == addPPlotAct:
+            self.addLeWidget(LePyplot, uid=None)
+        if action == addCheckboxAct:
+            self.addLeWidget(LeCheckBox, uid=None)
+        if action == addSliderAct:
+            self.addLeWidget(LeSlider, uid=None)
+        if action == addLabelAct:
+            self.addLeWidget(LeLabel, uid=None)
+        if action == rmSelfAct:
+            self.parent.removeLeWidget(self)
 
-    def addLeButton(self, uid=None):
+    def addLeWidget(self, type_, uid=None):
         if uid is None:
             uid = str(uuid.uuid4())
-        editable_button = LeButton(uid, 'Button {}'.format(self.leguiserver.count_widgets(type_=LeButton)), self)
-        self.leguiserver.all_editable_widgets.append(editable_button)
-        editable_button.clicked.connect(editable_button.on_click)
-        self.layout().addWidget(editable_button)
-        return editable_button
+        lewidget = type_(uid, '{} {}'.format(type_.__name__, self.leguiserver.count_widgets(type_=type_)), self, self.leguiserver)
+        self.leguiserver.all_editable_widgets.append(lewidget)
+        self.lewidgets.append(lewidget)
+        self.layout().addWidget(lewidget)
+        return lewidget
 
+    def removeLeWidget(self, lewidget):
+        self.leguiserver.all_editable_widgets.remove(lewidget)
+        self.lewidgets.remove(lewidget)
+        lewidget.close()
+
+    def removeAllLeWidgets(self):
+        for lewidget in self.lewidgets[:]:
+            self.removeLeWidget(lewidget)
+
+    def set_color(self, color):
+        if color is not None:
+            self.setAutoFillBackground(True)
+            palette = self.palette()
+            palette.setColor(QPalette.Window, QColor(color))
+            self.setPalette(palette)
+    
+    def get_depth(self):
+        return self.parent.get_depth() + 1
+
+    def set_label(self, label):
+        self.labelwidget = QLabel(text=label)
+        self.layout().addWidget(self.labelwidget)
+
+    def get_label(self):
+        return self.labelwidget.text()
+
+    def close(self):
+        self.removeAllLeWidgets()
+        super(LeLayout, self).close()
+
+
+class LeMainLayout(LeLayout):
+    def __init__(self, leguiserver):
+        super(LeLayout, self).__init__()
+        self.uid = "0"
+        self.parent = None
+        self.leguiserver = leguiserver
+        self.lewidgets = []
+        
+        self.setLayout(QHBoxLayout())
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor("white"))
+        self.setPalette(palette)
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        addVBoxAct = contextMenu.addAction("Add Vertical Layout")
+        addHBoxAct = contextMenu.addAction("Add Horizontal Layout")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == addVBoxAct:
+            self.addLeWidget(LeVBox, uid=None)
+        if action == addHBoxAct:
+            self.addLeWidget(LeHBox, uid=None)
+
+    def get_depth(self):
+        return 0
+
+class LeVBox(LeLayout):
+    def __init__(self, uid, label, parent, leguiserver):
+        super(LeLayout, self).__init__()
+        self.uid = uid
+        self.parent = parent
+        self.leguiserver = leguiserver
+        self.setLayout(QVBoxLayout())
+        self.lewidgets = []
+        
+        self.set_label(label)
+        self.labelwidget.setAlignment(QtCore.Qt.AlignCenter)
+        self.set_color(LeLayout.DEPTH_COLORS[self.get_depth()])
+
+class LeHBox(LeLayout):
+    def __init__(self, uid, label, parent, leguiserver):
+        super(LeLayout, self).__init__()
+        self.uid = uid
+        self.parent = parent
+        self.leguiserver = leguiserver
+        self.setLayout(QHBoxLayout())
+        self.lewidgets = []
+        
+        print("New Layout, depth {}".format(self.get_depth()))
+        self.set_label(label)
+        self.set_color(LeLayout.DEPTH_COLORS[self.get_depth()])       
 
 class LeButton(QPushButton):
-    def __init__(self, uid, text, parent):
+    def __init__(self, uid, label, parent, leguiserver):
         icon = None
-        super(LeButton, self).__init__(text=text) 
+        super(LeButton, self).__init__() 
+        self.leguiserver = leguiserver
         self.parent = parent
         self.uid = uid
+        self.set_label(label)
+        # Button specific
         self.was_clicked = False
 
     def contextMenuEvent(self, event):
         contextMenu = QMenu(self)
         deleteAct = contextMenu.addAction("Delete")
-        settextAct = contextMenu.addAction("Set Text")
+        settextAct = contextMenu.addAction("Set Label")
         action = contextMenu.exec_(self.mapToGlobal(event.pos()))
         if action == deleteAct:
-            self.parent.leguiserver.all_editable_widgets.remove(self)
-            self.close()
+            self.parent.removeLeWidget(self)
         if action == settextAct:
-            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new text:')
+            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new label:')
             if ok:
-                self.setText(text)
-            # Delete self TODO
-            # set text TODO
+                self.set_label(text)
     
+    def get_label(self):
+        return self.text()
+
+    def set_label(self, label):
+        self.setText(label)
+
+    # Button specific methods
     def on_click(self):
         self.was_clicked = True
 
@@ -106,9 +199,113 @@ class LeButton(QPushButton):
         self.was_clicked = False
         return status
 
+class LePyplot(FigureCanvasQTAgg):
+    def __init__(self, uid, label, parent, leguiserver, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(LePyplot, self).__init__(fig)
+        # LeWidget generic init
+        self.leguiserver = leguiserver
+        self.parent = parent
+        self.uid = uid
+        self.set_label(label)
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        deleteAct = contextMenu.addAction("Delete")
+        settextAct = contextMenu.addAction("Set Label")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == deleteAct:
+            self.parent.removeLeWidget(self)
+        if action == settextAct:
+            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new label:')
+            if ok:
+                self.set_label(text)
+    
+    def get_label(self):
+        return self.axes.get_title()
+
+    def set_label(self, label):
+        self.axes.set_title(label)
+
+
+class LeCheckBox(QCheckBox):
+    def __init__(self, uid, label, parent, leguiserver):
+        super(LeCheckBox, self).__init__()
+        self.leguiserver = leguiserver
+        self.parent = parent
+        self.uid = uid
+        self.set_label(label)
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        deleteAct = contextMenu.addAction("Delete")
+        settextAct = contextMenu.addAction("Set Label")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == deleteAct:
+            self.parent.removeLeWidget(self)
+        if action == settextAct:
+            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new label:')
+            if ok:
+                self.set_label(text)
+    
     def get_label(self):
         return self.text()
 
+    def set_label(self, label):
+        self.setText(label)
+
+class LeLabel(QLabel):
+    def __init__(self, uid, label, parent, leguiserver):
+        super(LeLabel, self).__init__()
+        self.leguiserver = leguiserver
+        self.parent = parent
+        self.uid = uid
+        self.set_label(label)
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        deleteAct = contextMenu.addAction("Delete")
+        settextAct = contextMenu.addAction("Set Label")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == deleteAct:
+            self.parent.removeLeWidget(self)
+        if action == settextAct:
+            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new label:')
+            if ok:
+                self.set_label(text)
+    
+    def get_label(self):
+        return self.text()
+
+    def set_label(self, label):
+        self.setText(label)
+
+class LeSlider(QSlider):
+    def __init__(self, uid, label, parent, leguiserver):
+        super(LeSlider, self).__init__()
+        self.leguiserver = leguiserver
+        self.parent = parent
+        self.uid = uid
+        self.set_label(label)
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        deleteAct = contextMenu.addAction("Delete")
+        settextAct = contextMenu.addAction("Set Label")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+        if action == deleteAct:
+            self.parent.removeLeWidget(self)
+        if action == settextAct:
+            text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter new label:')
+            if ok:
+                self.set_label(text)
+    
+    def get_label(self):
+        return self.label
+
+    def set_label(self, label):
+        self.label = label
 
 class LeGUIServer(object):
     def __init__(self, path=None):
@@ -121,30 +318,33 @@ class LeGUIServer(object):
         # Create and populate main window
         app = QtWidgets.QApplication(sys.argv)
         self.mainwindow = QMainWindow()
+        # set window size
+        self.mainwindow.resize(800, 600)
         self.mainwindow.show()
         btn = QPushButton('Edit', self.mainwindow)
         btn.clicked.connect(self.toggle_edit_mode)
         self.mainwindow.statusBar().addPermanentWidget(btn)
+        btn = QPushButton('Save', self.mainwindow)
+        self.mainwindow.statusBar().addPermanentWidget(btn)
         layout = QVBoxLayout()
 
+        mainlayout = LeMainLayout(self)
+        self.mainlayout = mainlayout
+        self.mainwindow.setCentralWidget(mainlayout)
 
-        editable = LeVBox(self, 'blue')
-        editable.setLayout(QVBoxLayout())
-        editable.addLeButton()
-        editable.addLeButton()
-        self.currently_selected_editablevbox = editable
+        if True:
+            editable = self.mainlayout.addLeWidget(LeVBox)
+            editable.addLeWidget(LeButton)
+            editable.addLeWidget(LeButton)
+            editable.addLeWidget(LeSlider)
+            editable.addLeWidget(LeCheckBox)
+            editable.addLeWidget(LeLabel)
+            self.currently_selected_editablevbox = editable
 
-        editable2 = LeVBox(self, 'cyan')
-        editable2.setLayout(QVBoxLayout())
-        editable2.layout().addWidget(MplCanvas(self, width=5, height=4, dpi=100))
+            editable2 = self.mainlayout.addLeWidget(LeVBox)
+            editable2.addLeWidget(LePyplot)
 
 
-        parent = MainLayoutWidget('red')
-        parent.setLayout(QHBoxLayout())
-        parent.layout().addWidget(editable)
-
-        parent.layout().addWidget(editable2)
-        self.mainwindow.setCentralWidget(parent)
 
         s = rospy.Service('get_widget', QuestionAnswer, self.get_widget_service_call)
         s = rospy.Service('is_button_clicked', QuestionAnswer, self.is_button_clicked_service_call)
@@ -155,6 +355,8 @@ class LeGUIServer(object):
         timer.start(1000)
 
         app.exec_()
+
+
     
     def toggle_edit_mode(self):
         self.edit_mode_enabled = not self.edit_mode_enabled
